0f844cb2a69ac780644278cbcd5768956b8c0758
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-07-24 14:37:18 +0530

    Code Optemization

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,62 +1,66 @@
 import { Component } from "react";
+import styled from "styled-components";
+
+const Nav = styled.div`
+  width: 100%;
+  height: 70px;
+  background: linear-gradient(170deg, #1bc059, #0d47a1);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+`;
+
+const Title = styled.h1`
+  font-size: 30px;
+  color: #fff;
+  font-weight: 600;
+  font-family: "Montserrat", sans-serif;
+  text-transform: uppercase;
+  margin-left: 20px;
+  &:hover {
+    color: #0f0;
+  }
+`;
+
+const CartContainer = styled.div`
+  position: relative;
+  cursor: pointer;
+`;
+
+const CartIcon = styled.img`
+  height: 48px;
+  margin-right: 20px;
+`;
+
+const CartCount = styled.span`
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  padding: 4px 8px;
+  position: absolute;
+  right: 10px;
+  top: -5px;
+  font-size: 12px;
+  visibility: ${(props) => (props.show ? "visible" : "hidden")};
+`;
 
 export default class Navbar extends Component {
   render() {
     return (
       <>
-        <div className="navbar" style={styles.nav}>
-          <div className="app-title">
-            <h1 style={styles.title}>MovieFlix</h1>
-          </div>
-          <div className="cart" style={styles.cartIconContainer}>
-            <img
+        <Nav className="navbar">
+          <Title className="app-title">MovieFlix</Title>
+          <CartContainer className="cart">
+            <CartIcon
               src="https://cdn-icons-png.flaticon.com/128/891/891462.png"
               alt="cart-icon"
-              style={styles.cartIcon}
             />
-            <span className="cart-count" style={styles.cartCount}>
+            <CartCount className="cart-count" color="Yellow" show={true}>
               0
-            </span>
-          </div>
-        </div>
+            </CartCount>
+          </CartContainer>
+        </Nav>
       </>
     );
   }
 }
-
-const styles = {
-  cartIcon: {
-    height: 48,
-    marginRight: 20,
-  },
-  nav: {
-    height: 70,
-    background: "#4267b2",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    position: "relative",
-  },
-  title: {
-    fontSize: 30,
-    color: "#fff",
-    fontWeight: 600,
-    fontFamily: '"Montserrat", sans-serif',
-    textTransform: "uppercase",
-    marginLeft: 20,
-  },
-
-  cartIconContainer: {
-    position: "relative",
-    cursor: "pointer",
-  },
-  cartCount: {
-    background: "orange",
-    borderRadius: "50%",
-    padding: "4px 8px",
-    position: "absolute",
-    right: 10,
-    top: -5,
-    fontSize: 12,
-  },
-};
